d37666730b2eb368e20956c26eba36a86cbc6c63
Vinicius de Ribeiro <123048968+Nyssim@users.noreply.github.com>
2024-08-14 21:19:15 -0300

    Criação do layout do programa

---
+++ Produto/src/main/java/com/produto/modelo/ProdutoModel/JTableProduto.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.produto.modelo.ProdutoModel;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Aluno
+ */
+public class JTableProduto extends AbstractTableModel {
+    
+    //Atriutos da Tabela
+    
+    private ArrayList<ProdutoModel>  listaDeProduto;
+    private String[] colunas = {"Código","Produto","Código de Barras", "Unidade"};
+    
+    //Metodos da Tabela
+    
+    //Criar a lista que vai agrupar os dados da Tabela
+    public JTableProduto() {
+        this.listaDeProduto = new ArrayList<>();
+    }
+    
+    //Metodo adicionar
+    public void addProduto(ProdutoModel func) {
+        this.listaDeProduto.add( func);
+        fireTableDataChanged();
+    }
+    
+    //Metodo deletar
+    public void delProduto(int linha) {
+        this.listaDeProduto.remove( linha);
+        fireTableDataChanged();
+    }
+    
+    public void altProduto(int linha) {
+        
+    }
+    
+    public void inaProduto(int linha) {
+        
+    }
+    
+    public void atiProduto(int linha) {
+        
+    }
+
+    @Override
+    public int getRowCount() {
+        return listaDeProduto.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                 return this.listaDeProduto.get(rowIndex).getCodigo();
+            case 1:
+                return this.listaDeProduto.get(rowIndex).getCodigoBarra();
+            case 2:
+                return this.listaDeProduto.get(rowIndex).getNome();
+            case 3:
+                return this.listaDeProduto.get(rowIndex).getUnidade();
+                
+            default:
+                return this.listaDeProduto.get(rowIndex);
+        }
+    }
+    
+    @Override
+    public String getColumnName(int columnIndex){
+        return this.colunas[columnIndex];
+    }
+}
---
+++ Produto/src/main/java/com/produto/modelo/ProdutoModel/ProdutoModel.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.produto.modelo.ProdutoModel;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ProdutoModel {
+    
+    //Atributos do Produto
+    private String codigo;
+    private String codigoBarra;
+    private String nome;
+    private int unidade;
+    private double peso;
+    private boolean estado;
+
+    //Encapsulamento dos atributos dos produtos
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the codigoBarra
+     */
+    public String getCodigoBarra() {
+        return codigoBarra;
+    }
+
+    /**
+     * @param codigoBarra the codigoBarra to set
+     */
+    public void setCodigoBarra(String codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the unidade
+     */
+    public int getUnidade() {
+        return unidade;
+    }
+
+    /**
+     * @param unidade the unidade to set
+     */
+    public void setUnidade(int unidade) {
+        this.unidade = unidade;
+    }
+
+    /**
+     * @return the peso
+     */
+    public double getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the estado
+     */
+    public boolean isEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+}
